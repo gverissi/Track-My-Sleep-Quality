29bc2374970fb2e10b8a937b237dab7f59418c1b
greg <greg.verissimo@gmail.com>
2020-05-22 01:25:46 +0200

    12. Exercise: Refresh Data with DiffUtil

--- app/src/main/java/com/gregcorp/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ app/src/main/java/com/gregcorp/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -6,24 +6,25 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.gregcorp.trackmysleepquality.R
 import com.gregcorp.trackmysleepquality.convertDurationToFormatted
 import com.gregcorp.trackmysleepquality.convertNumericQualityToString
 import com.gregcorp.trackmysleepquality.database.SleepNight
 
-class SleepNightAdapter : RecyclerView.Adapter<SleepNightAdapter.ViewHolder>() {
-
-    var data = listOf<SleepNight>()
-        set(value) {
-            field = value
-            notifyDataSetChanged()
-        }
+class SleepNightAdapter : ListAdapter<SleepNight, SleepNightAdapter.ViewHolder>(SleepNightDiffCallback()) {
 
     /**
-     * Part of the RecyclerView adapter, called when RecyclerView needs to know the size of the list
+     * Part of the RecyclerView adapter, called when RecyclerView needs a new [ViewHolder]
+     *
+     * A ViewHolder holds a view for the [RecyclerView] as well as providing additional informations
+     * to the RecyclerView such as where on the screen it was last drawn during scrolling.
      */
-    override fun getItemCount() = data.size
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder.from(parent)
+    }
 
     /**
      * Part of the RecyclerView adapter, called when RecyclerView needs to show an item.
@@ -32,18 +33,8 @@ class SleepNightAdapter : RecyclerView.Adapter<SleepNightAdapter.ViewHolder>() {
      * may have been set previously.
      */
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val item = data[position]
+        val item = getItem(position)
         holder.bind(item)
-    }
-
-    /**
-     * Part of the RecyclerView adapter, called when RecyclerView needs a new [ViewHolder]
-     *
-     * A ViewHolder holds a view for thr [RecyclerView] as well as providing additional informations
-     * to the RecyclerView such as where on the screen it was last drawn during scrolling.
-     */
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder.from(parent)
     }
 
     class ViewHolder private constructor(itemView: View): RecyclerView.ViewHolder(itemView) {
@@ -74,6 +65,16 @@ class SleepNightAdapter : RecyclerView.Adapter<SleepNightAdapter.ViewHolder>() {
                 val view = layoutInflater.inflate(R.layout.list_item_sleep_night, parent, false)
                 return ViewHolder(view)
             }
+        }
+    }
+
+    class SleepNightDiffCallback : DiffUtil.ItemCallback<SleepNight>() {
+        override fun areItemsTheSame(oldItem: SleepNight, newItem: SleepNight): Boolean {
+            return oldItem.nightId == newItem.nightId
+        }
+
+        override fun areContentsTheSame(oldItem: SleepNight, newItem: SleepNight): Boolean {
+            return oldItem == newItem
         }
     }
 
