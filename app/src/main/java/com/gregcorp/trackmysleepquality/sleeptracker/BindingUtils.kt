7bc5830298518935ba6c4c62632059bb7fc54e4f
greg <greg.verissimo@gmail.com>
2020-05-22 02:58:40 +0200

    14. Exercise: Add Binding Adapters

---
+++ app/src/main/java/com/gregcorp/trackmysleepquality/sleeptracker/BindingUtils.kt
@@ -0,0 +1,39 @@
+package com.gregcorp.trackmysleepquality.sleeptracker
+
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+import com.gregcorp.trackmysleepquality.R
+import com.gregcorp.trackmysleepquality.convertDurationToFormatted
+import com.gregcorp.trackmysleepquality.convertNumericQualityToString
+import com.gregcorp.trackmysleepquality.database.SleepNight
+
+
+@BindingAdapter("sleepDurationFormatted") // value to use in the xml file
+fun TextView.setSleepDurationFormatted(item: SleepNight?) {
+    item?.let {
+        text = convertDurationToFormatted(item.startTimeMilli, item.endTimeMilli, context.resources)
+    }
+}
+
+@BindingAdapter("sleepQualityString")
+fun TextView.setSleepQualityString(item: SleepNight?) {
+    item?.let {
+        text = convertNumericQualityToString(item.sleepQuality, context.resources)
+    }
+}
+
+@BindingAdapter("sleepImage")
+fun ImageView.setSleepImage(item: SleepNight) {
+    setImageResource(
+        when (item.sleepQuality) {
+            0 -> R.drawable.ic_sleep_0
+            1 -> R.drawable.ic_sleep_1
+            2 -> R.drawable.ic_sleep_2
+            3 -> R.drawable.ic_sleep_3
+            4 -> R.drawable.ic_sleep_4
+            5 -> R.drawable.ic_sleep_5
+            else -> R.drawable.ic_sleep_active
+        }
+    )
+}
